b7cdfb4bb78c3b2515f9a7ab31bc4ec3bbf39a63
𓆩 𝐈𝐋𝐘𝐀𝐒𝐒 𓆪 <139225454+FasterExE@users.noreply.github.com>
2024-04-20 03:59:21 +0100

    Update slhostdns.sh

--- ssh/slhostdns.sh
+++ ssh/slhostdns.sh
@@ -43,7 +43,8 @@ RESULT=$(curl -sLX PUT "https://api.cloudflare.com/client/v4/zones/${ZONE}/dns_r
      -H "X-Auth-Key: ${CF_KEY}" \
      -H "Content-Type: application/json" \
      --data '{"type":"A","name":"'${SUB_DOMAIN}'","content":"'${IP}'","ttl":120,"proxied":false}')
-echo "Updating DNS NS for ${NS_DOMAIN}..."
+     
+echo "Updating DNS NS for ${CF_DOMAIN}..."
 ZONE=$(curl -sLX GET "https://api.cloudflare.com/client/v4/zones?name=${DOMAIN}&status=active" \
      -H "X-Auth-Email: ${CF_ID}" \
      -H "X-Auth-Key: ${CF_KEY}" \
