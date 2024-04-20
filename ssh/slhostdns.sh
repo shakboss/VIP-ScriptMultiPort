6eb70471d07e2e21bf796f0777eef462bdd44e4b
𓆩 𝐈𝐋𝐘𝐀𝐒𝐒 𓆪 <139225454+FasterExE@users.noreply.github.com>
2024-04-20 03:57:16 +0100

    Update slhostdns.sh

--- ssh/slhostdns.sh
+++ ssh/slhostdns.sh
@@ -8,11 +8,11 @@ rm -rf /root/nsdomain
 rm nsdomain
 
 sub=$(</dev/urandom tr -dc a-z0-9 | head -c5)
-subsl=$(</dev/urandom tr -dc a-x0-9 | head -c5)
 
 DOMAIN=ilyass.my.id
 SUB_DOMAIN=${sub}.multi-script.ilyass.my.id
-NS_DOMAIN=slowdns-${subsl}.multi-script.ilyass.my.id
+CF_DOMAIN=flare-${sub}.multi-script.ilyass.my.id
+NS_DOMAIN=slowdns-${sub}.multi-script.ilyass.my.id
 
 CF_ID=ilyassnajhi10@gmail.com
 CF_KEY=c802b86ce41793d4aed457ad769889b7e31e9
@@ -49,6 +49,25 @@ ZONE=$(curl -sLX GET "https://api.cloudflare.com/client/v4/zones?name=${DOMAIN}&
      -H "X-Auth-Key: ${CF_KEY}" \
      -H "Content-Type: application/json" | jq -r .result[0].id)
 
+RECORD=$(curl -sLX GET "https://api.cloudflare.com/client/v4/zones/${ZONE}/dns_records?name=${CF_DOMAIN}" \
+     -H "X-Auth-Email: ${CF_ID}" \
+     -H "X-Auth-Key: ${CF_KEY}" \
+     -H "Content-Type: application/json" | jq -r .result[0].id)
+
+if [[ "${#RECORD}" -le 10 ]]; then
+     RECORD=$(curl -sLX POST "https://api.cloudflare.com/client/v4/zones/${ZONE}/dns_records" \
+     -H "X-Auth-Email: ${CF_ID}" \
+     -H "X-Auth-Key: ${CF_KEY}" \
+     -H "Content-Type: application/json" \
+     --data '{"type":"CNAME","name":"'${CF_DOMAIN}'","content":"'${SUB_DOMAIN}'","ttl":120,"proxied":true}' | jq -r .result.id)
+fi
+
+echo "Updating DNS NS for ${NS_DOMAIN}..."
+ZONE=$(curl -sLX GET "https://api.cloudflare.com/client/v4/zones?name=${DOMAIN}&status=active" \
+     -H "X-Auth-Email: ${CF_ID}" \
+     -H "X-Auth-Key: ${CF_KEY}" \
+     -H "Content-Type: application/json" | jq -r .result[0].id)
+
 RECORD=$(curl -sLX GET "https://api.cloudflare.com/client/v4/zones/${ZONE}/dns_records?name=${NS_DOMAIN}" \
      -H "X-Auth-Email: ${CF_ID}" \
      -H "X-Auth-Key: ${CF_KEY}" \
@@ -75,4 +94,5 @@ echo $SUB_DOMAIN > /root/domain
 echo $SUB_DOMAIN > /root/scdomain
 echo $SUB_DOMAIN > /etc/xray/domain
 echo $SUB_DOMAIN > /etc/xray/scdomain
+echo $CF_DOMAIN > /etc/xray/cfdomain
 echo "IP=$SUB_DOMAIN" > /var/lib/alexxa-pro/ipvps.conf
