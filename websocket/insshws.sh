d8c35ed218e6170f9bc7cd39f5e07f4b7ea76041
𓆩 𝐈𝐋𝐘𝐀𝐒𝐒 𓆪 <139225454+FasterExE@users.noreply.github.com>
2024-04-20 09:28:41 +0100

    Update insshws.sh

--- websocket/insshws.sh
+++ websocket/insshws.sh
@@ -4,6 +4,7 @@ cd
 
 wget -O /usr/local/bin/ws-dropbear https://raw.githubusercontent.com/FasterExE/VIP-ScriptMultiPort/main/websocket/dropbear-ws.py
 wget -O /usr/local/bin/ws-stunnel https://raw.githubusercontent.com/FasterExE/VIP-ScriptMultiPort/main/websocket/ws-stunnel
+wget -O /usr/local/bin/ws-ovpn https://raw.githubusercontent.com/FasterExE/VIP-ScriptMultiPort/main/websocket/ovpn-ws.py
 
 chmod +x /usr/local/bin/ws-dropbear
 chmod +x /usr/local/bin/ws-stunnel
@@ -12,6 +13,8 @@ wget -O /etc/systemd/system/ws-dropbear.service https://raw.githubusercontent.co
 
 wget -O /etc/systemd/system/ws-stunnel.service https://raw.githubusercontent.com/FasterExE/VIP-ScriptMultiPort/main/websocket/ws-stunnel.service && chmod +x /etc/systemd/system/ws-stunnel.service
 
+wget -O /etc/systemd/system/ws-ovpn.service https://raw.githubusercontent.com/FasterExE/VIP-ScriptMultiPort/main/websocket/service-wsovpn && chmod +x /etc/systemd/system/ws-ovpn.service
+
 systemctl daemon-reload
 systemctl enable ws-dropbear.service
 systemctl start ws-dropbear.service
@@ -19,3 +22,6 @@ systemctl restart ws-dropbear.service
 systemctl enable ws-stunnel.service
 systemctl start ws-stunnel.service
 systemctl restart ws-stunnel.service
+systemctl enable ws-ovpn.service
+systemctl start ws-ovpn.service
+systemctl restart ws-ovpn.service
