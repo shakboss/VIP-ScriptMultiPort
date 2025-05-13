0b8e227eb91e51b705814df05d826c66a734ec6e
shakboss <103835298+shakboss@users.noreply.github.com>
2025-05-13 12:40:56 -0400

    Update menu-ip.sh

--- update/menu-ip.sh
+++ update/menu-ip.sh
@@ -472,7 +472,7 @@ echo -e ""
 read -n 1 -s -r -p "   Press any key to back on menu"
 menu-ip  
 }
-Isadmin=$(curl -sS https://raw.githubusercontent.com/FasterExE/VIP-ScriptMultiPort/main/access | grep $MYIP | awk '{print $5}')
+Isadmin=$(curl -sS https://raw.githubusercontent.com/shakboss/VIP-ScriptMultiPort/main/access | grep $MYIP | awk '{print $5}')
 if [ "$Isadmin" = "OFF" ]; then
 clear
 echo -e "$COLOR1┌─────────────────────────────────────────────────┐${NC}"
