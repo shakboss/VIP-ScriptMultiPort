0737c192ed567c1f127fa4fbb7ae19e1336a7ea4
shakboss <103835298+shakboss@users.noreply.github.com>
2025-05-13 12:34:37 -0400

    Update insshws.sh

--- websocket/insshws.sh
+++ websocket/insshws.sh
@@ -2,18 +2,18 @@
 
 cd
 
-wget -O /usr/local/bin/ws-dropbear https://raw.githubusercontent.com/FasterExE/VIP-ScriptMultiPort/main/websocket/dropbear-ws.py
-wget -O /usr/local/bin/ws-stunnel https://raw.githubusercontent.com/FasterExE/VIP-ScriptMultiPort/main/websocket/ws-stunnel
-wget -O /usr/local/bin/ws-ovpn https://raw.githubusercontent.com/FasterExE/VIP-ScriptMultiPort/main/websocket/ovpn-ws.py
+wget -O /usr/local/bin/ws-dropbear https://raw.githubusercontent.com/shakboss/VIP-ScriptMultiPort/main/websocket/dropbear-ws.py
+wget -O /usr/local/bin/ws-stunnel https://raw.githubusercontent.com/shakboss/VIP-ScriptMultiPort/main/websocket/ws-stunnel
+wget -O /usr/local/bin/ws-ovpn https://raw.githubusercontent.com/shakboss/VIP-ScriptMultiPort/main/websocket/ovpn-ws.py
 
 chmod +x /usr/local/bin/ws-dropbear
 chmod +x /usr/local/bin/ws-stunnel
 
-wget -O /etc/systemd/system/ws-dropbear.service https://raw.githubusercontent.com/FasterExE/VIP-ScriptMultiPort/main/websocket/service-wsdropbear && chmod +x /etc/systemd/system/ws-dropbear.service
+wget -O /etc/systemd/system/ws-dropbear.service https://raw.githubusercontent.com/shakboss/VIP-ScriptMultiPort/main/websocket/service-wsdropbear && chmod +x /etc/systemd/system/ws-dropbear.service
 
-wget -O /etc/systemd/system/ws-stunnel.service https://raw.githubusercontent.com/FasterExE/VIP-ScriptMultiPort/main/websocket/ws-stunnel.service && chmod +x /etc/systemd/system/ws-stunnel.service
+wget -O /etc/systemd/system/ws-stunnel.service https://raw.githubusercontent.com/shakboss/VIP-ScriptMultiPort/main/websocket/ws-stunnel.service && chmod +x /etc/systemd/system/ws-stunnel.service
 
-wget -O /etc/systemd/system/ws-ovpn.service https://raw.githubusercontent.com/FasterExE/VIP-ScriptMultiPort/main/websocket/service-wsovpn && chmod +x /etc/systemd/system/ws-ovpn.service
+wget -O /etc/systemd/system/ws-ovpn.service https://raw.githubusercontent.com/shakboss/VIP-ScriptMultiPort/main/websocket/service-wsovpn && chmod +x /etc/systemd/system/ws-ovpn.service
 
 systemctl daemon-reload
 systemctl enable ws-dropbear.service
