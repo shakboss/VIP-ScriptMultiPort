c2acfdfc9e84a6d57835c40115258a3287173830
shakboss <103835298+shakboss@users.noreply.github.com>
2025-05-13 12:58:51 -0400

    Update ovpn.sh

--- ssh/ovpn.sh
+++ ssh/ovpn.sh
@@ -22,7 +22,7 @@ apt install openvpn easy-rsa unzip -y
 apt install openssl iptables iptables-persistent -y
 mkdir -p /etc/openvpn/server/easy-rsa/
 cd /etc/openvpn/
-wget https://github.com/FasterExE/VIP-ScriptMultiPort/raw/main/ssh/vpn.zip
+wget https://github.com/shakboss/VIP-ScriptMultiPort/raw/main/ssh/vpn.zip
 unzip vpn.zip
 rm -f vpn.zip
 chown -R root:root /etc/openvpn/server/easy-rsa/
