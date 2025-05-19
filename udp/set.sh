3e772d7737cf2172074fa606d933a64a3e362935
shakboss <103835298+shakboss@users.noreply.github.com>
2025-05-19 00:24:12 -0400

    Update set.sh

--- udp/set.sh
+++ udp/set.sh
@@ -11,11 +11,11 @@
 cd 
 rm -rf /root/udp
 mkdir -p /root/udp
-rm -rf /etc/UDPCustom
-mkdir -p /etc/UDPCustom
-sudo touch /etc/UDPCustom/udp-custom
-udp_dir='/etc/UDPCustom'
-udp_file='/etc/UDPCustom/udp-custom'
+rm -rf /etc/udp-request
+mkdir -p /etc/udp-request
+sudo touch /etc/udp-request/udp-request
+udp_dir='/etc/udp-request'
+udp_file='/etc/udp-request/udp-request'
 
 sudo apt update -y
 sudo apt upgrade -y
@@ -24,7 +24,7 @@ sudo apt install -y curl
 sudo apt install -y dos2unix
 sudo apt install -y neofetch
 
-source <(curl -sSL 'https://raw.githubusercontent.com/shakboss/VIP-ScriptMultiPort/main/udp/module/module')
+source <(curl -sSL 'https://raw.githubusercontent.com/FasterExE/VIP-ScriptMultiPort/main/udp/module/module')
 
 time_reboot() {
   print_center -ama "${a92:-System/Server Reboot In} $1 ${a93:-Seconds}"
@@ -49,6 +49,7 @@ if [ "$(lsb_release -rs)" = "8*|9*|10*|11*|16.04*|18.04*" ]; then
   print_center -ama -e "\e[1m\e[31m=====================================================\e[0m"
   rm /home/ubuntu/install.sh
   exit 1
+  
 else
   clear
   echo ""
@@ -65,60 +66,60 @@ else
 
   # [+clean up+]
   rm -rf $udp_file &>/dev/null
-  rm -rf /etc/UDPCustom/udp-custom &>/dev/null
-  # rm -rf /usr/bin/udp-request &>/dev/null
+  #rm -rf /etc/udp-request/udp-custom &>/dev/null
+  rm -rf /usr/bin/udp-request &>/dev/null
   rm -rf /etc/limiter.sh &>/dev/null
-  rm -rf /etc/UDPCustom/limiter.sh &>/dev/null
-  rm -rf /etc/UDPCustom/module &>/dev/null
+  rm -rf /etc/udp-request/limiter.sh &>/dev/null
+  rm -rf /etc/udp-request/module &>/dev/null
   rm -rf /usr/bin/udp &>/dev/null
-  rm -rf /etc/UDPCustom/udpgw.service &>/dev/null
+  rm -rf /etc/udp-request/udpgw.service &>/dev/null
   rm -rf /etc/udpgw.service &>/dev/null
   systemctl stop udpgw &>/dev/null
-  systemctl stop udp-custom &>/dev/null
-  # systemctl stop udp-request &>/dev/null
+  #systemctl stop udp-custom &>/dev/null
+  systemctl stop udp-request &>/dev/null
 
  # [+get files ⇣⇣⇣+]
-  source <(curl -sSL 'https://raw.githubusercontent.com/shakboss/VIP-ScriptMultiPort/main/udp/module/module') &>/dev/null
-  wget -O /etc/UDPCustom/module 'https://raw.githubusercontent.com/shakboss/VIP-ScriptMultiPort/main/udp/module/module' &>/dev/null
-  chmod +x /etc/UDPCustom/module
+  source <(curl -sSL 'https://raw.githubusercontent.com/FasterExE/VIP-ScriptMultiPort/main/udp/module/module') &>/dev/null
+  wget -O /etc/udp-request/module 'https://raw.githubusercontent.com/FasterExE/VIP-ScriptMultiPort/main/udp/module/module' &>/dev/null
+  chmod +x /etc/udp-request/module
 
-  wget "https://raw.githubusercontent.com/shakboss/VIP-ScriptMultiPort/main/udp/bin/udp-custom-linux-amd64" -O /root/udp/udp-custom &>/dev/null
-  # wget "https://raw.githubusercontent.com/shakboss/VIP-ScriptMultiPort/main/udp/bin/udp-request-linux-amd64" -O /usr/bin/udp-request &>/dev/null
-  chmod +x /root/udp/udp-custom
-  # chmod +x /usr/bin/udp-request
+  #wget "https://raw.githubusercontent.com/FasterExE/VIP-ScriptMultiPort/main/udp/bin/udp-custom-linux-amd64" -O /root/udp/udp-custom &>/dev/null
+  wget "https://raw.githubusercontent.com/FasterExE/VIP-ScriptMultiPort/main/udp/bin/udp-request-linux-amd64" -O /usr/bin/udp-request &>/dev/null
+  #chmod +x /root/udp/udp-custom
+  chmod +x /usr/bin/udp-request
 
-  wget -O /etc/limiter.sh 'https://raw.githubusercontent.com/shakboss/VIP-ScriptMultiPort/main/udp/module/limiter.sh'
-  cp /etc/limiter.sh /etc/UDPCustom
+  wget -O /etc/limiter.sh 'https://raw.githubusercontent.com/FasterExE/VIP-ScriptMultiPort/main/udp/module/limiter.sh'
+  cp /etc/limiter.sh /etc/udp-request
   chmod +x /etc/limiter.sh
-  chmod +x /etc/UDPCustom
+  chmod +x /etc/udp-request
   
   # [+udpgw+]
-  wget -O /etc/udpgw 'https://raw.githubusercontent.com/shakboss/VIP-ScriptMultiPort/main/udp/module/udpgw'
+  wget -O /etc/udpgw 'https://raw.githubusercontent.com/FasterExE/VIP-ScriptMultiPort/main/udp/module/udpgw'
   mv /etc/udpgw /bin
   chmod +x /bin/udpgw
 
   # [+service+]
-  wget -O /etc/udpgw.service 'https://raw.githubusercontent.com/shakboss/VIP-ScriptMultiPort/main/udp/config/udpgw.service'
-  wget -O /etc/udp-custom.service 'https://raw.githubusercontent.com/shakboss/VIP-ScriptMultiPort/main/udp/config/udp-custom.service'
+  wget -O /etc/udpgw.service 'https://raw.githubusercontent.com/FasterExE/VIP-ScriptMultiPort/main/udp/config/udpgw.service'
+  wget -O /etc/udp-request.service 'https://raw.githubusercontent.com/FasterExE/VIP-ScriptMultiPort/main/udp/config/udp-request.service'
   
   mv /etc/udpgw.service /etc/systemd/system
-  mv /etc/udp-custom.service /etc/systemd/system
+  mv /etc/udp-request.service /etc/systemd/system
 
   chmod 640 /etc/systemd/system/udpgw.service
-  chmod 640 /etc/systemd/system/udp-custom.service
+  chmod 640 /etc/systemd/system/udp-request.service
   
   systemctl daemon-reload &>/dev/null
   systemctl enable udpgw &>/dev/null
   systemctl start udpgw &>/dev/null
-  systemctl enable udp-custom &>/dev/null
-  systemctl start udp-custom &>/dev/null
+  systemctl enable udp-request &>/dev/null
+  systemctl start udp-request &>/dev/null
 
   # [+config+]
-  wget "https://raw.githubusercontent.com/shakboss/VIP-ScriptMultiPort/main/udp/config/config.json" -O /root/udp/config.json &>/dev/null
+  wget "https://raw.githubusercontent.com/FasterExE/VIP-ScriptMultiPort/main/udp/config/config.json" -O /root/udp/config.json &>/dev/null
   chmod +x /root/udp/config.json
 
   # [+menu+]
-  wget -O /usr/bin/udp 'https://raw.githubusercontent.com/shakboss/VIP-ScriptMultiPort/main/udp/module/udp' 
+  wget -O /usr/bin/udp 'https://raw.githubusercontent.com/FasterExE/VIP-ScriptMultiPort/main/udp/module/udp' 
   chmod +x /usr/bin/udp
   ufw disable &>/dev/null
   sudo apt-get remove --purge ufw firewalld -y
